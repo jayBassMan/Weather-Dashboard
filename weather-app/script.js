afc259b5d8286be30a98843a5ad9a2e0856e723e
Jonathan Johnson <jonathandila682@gmail.com>
2022-03-16 09:36:44 -0500

    Add html,css,js

---
+++ weather-app/script.js
@@ -0,0 +1,39 @@
+// GIVEN a weather dashboard with form inputs
+var card_1 = document.querySelector(".card-1")
+var card_2 = document.querySelector(".card-2")
+var card_3 = document.querySelector(".card-3")
+var card_4 = document.querySelector(".card-4")
+var btn = onClick(btn);
+// WHEN I search for a city
+
+function citySearch() {
+
+  }
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+function furtureConditions() { 
+    
+ }
+// WHEN I view current weather conditions for that city
+function veiwWeatherConditions() {
+
+}
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+fetch api function
+// WHEN I view the UV index
+function veiwUVIndex() {}
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+function colorCode(){
+
+}
+// WHEN I view future weather conditions for that city
+function future() {
+
+}
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+function fiveDayForcast(){
+
+}
+// WHEN I click on a city in the search history
+btn.document.onClick
+// THEN I am again presented with current and future conditions for that city
+future() 
